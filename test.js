918ecc5f7e2acb105b8a8f3079dc0fd98be72348
Lyranelia <lyranelia@disroot.org>
2026-09-21 11:20:39 +0200

    added funtionality to console screen: admin screen, returning vehicles, put all 'screens' in functions for readability

--- test.js
+++ test.js
@@ -2,21 +2,8 @@
 // This is gonna be a test file for TypeScript 
 // prompt am Arsch, muss zu readline wechseln
 Object.defineProperty(exports, "__esModule", { value: true });
-const nodeFs = require("node:fs");
+const nodeFs = require("node:fs"); // hä wo kommt'n das her?
 const readlineSync = require("readline-sync");
-/*// Create an interface for input and output
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-const askQuestion = (question: string) => {
-    return new Promise((resolve) => {
-        rl.question(question, (answer: string) => {
-            resolve(answer);
-        });
-    });
-};*/
 class TwoWheelVehicle {
     type;
     name;
@@ -54,76 +41,215 @@ class ElectricVehicle extends TwoWheelVehicle {
     }
 }
 class EScooter extends ElectricVehicle {
-    constructor(name, pricePerMinute, battery = 100) {
-        super("E-Scooter", name, pricePerMinute, battery);
+    static pricePerMinute = 0.09;
+    constructor(name, battery = 100) {
+        super("E-Scooter", name, EScooter.pricePerMinute, battery);
     }
 }
 class EBike extends ElectricVehicle {
-    constructor(name, pricePerMinute, battery = 100) {
-        super("E-Bike", name, pricePerMinute, battery);
+    static pricePerMinute = 0.11;
+    constructor(name, battery = 100) {
+        super("E-Bike", name, EBike.pricePerMinute, battery);
     }
 }
 class Bike extends TwoWheelVehicle {
-    constructor(name, pricePerMinute) {
-        super("Bike", name, pricePerMinute);
+    static pricePerMinute = 0.05;
+    constructor(name) {
+        super("Bike", name, Bike.pricePerMinute);
     }
 }
 let vehicles = [
-    new EScooter("Xiaomi 1 Pro", 0.08),
-    new EScooter("Xiaomi 2 Pro", 0.08),
-    new EScooter("Xiaomi 5 Pro", 0.09),
-    new EScooter("Xiaomi 1 ", 0.07),
-    new EBike("Decathlon Rockrider", 0.12),
-    new EBike("Decathlon 2", 0.11),
-    new EBike("Decathlon 3", 0.12),
-    new EBike("Xiaomi 4", 0.13),
-    new Bike("Fahrrad Marke 4 Pro", 0.06),
-    new Bike("Fahrrad Marke 3 Pro", 0.05),
-    new Bike("Fahrrad Marke 4 ", 0.05),
-    new Bike("Fahrrad Marke 4 Lite", 0.05)
+    new EScooter("Xiaomi 1 Pro"),
+    new EScooter("Xiaomi 2 Pro"),
+    new EScooter("Xiaomi 5 Pro"),
+    new EScooter("Xiaomi 1 "),
+    new EBike("Decathlon Rockrider"),
+    new EBike("Decathlon 2"),
+    new EBike("Decathlon 3"),
+    new EBike("Xiaomi 4"),
+    new Bike("Fahrrad Marke 4 Pro"),
+    new Bike("Fahrrad Marke 3 Pro"),
+    new Bike("Fahrrad Marke 4 "),
+    new Bike("Fahrrad Marke 4 Lite")
 ];
+let eVehicles = [];
 // Hervorragend, das funktioniert schonmal
 // dann jetzt Methoden, wa
 // und ne Dublettenprüfung für die IDs wär auch ganz sinnvoll
 // console.log(vehicles)
 function logo() {
-    console.log("\n+++++++++++++++++++++++++++++");
-    console.log("+     SCOOTERS; EBIKES      +");
-    console.log("+          & BIKES          +");
-    console.log("+++++++++++++++++++++++++++++\n\n");
+    console.log("\n  +++++++++++++++++++++++++++++");
+    console.log("  +     SCOOTERS; EBIKES      +");
+    console.log("  +          & BIKES          +");
+    console.log("  +++++++++++++++++++++++++++++\n\n");
 }
-function logType(type) {
-    console.log(`You want to rent an ${type}, is that right?`);
+function adminScreen(name) {
+    let isTypeNull = true;
+    console.log(`Welcome to the admin window, ${name}!`);
+    while (isTypeNull) {
+        console.log("What do you want to do?");
+        let adminQuestion = readlineSync.question("[1] show all vehicles\n[2] show all vehicles, detailed\n[3] show all rentable vehicles\n[4] charge all to 100 %\n[5] add new vehicle\n\nchoice: ");
+        const adminNum = parseInt(adminQuestion);
+        if (adminQuestion === null || isNaN(adminNum) || adminNum < 1 || adminNum > 5) {
+            console.log("Please enter 1, 2, 3, 4 or 5.");
+            continue;
+        }
+        isTypeNull = false;
+        switch (adminNum) {
+            case 1:
+                for (let i = 0; i < vehicles.length; i++) {
+                    let vehicle = vehicles[i];
+                    if (vehicle) {
+                        console.log(`${vehicle.type} - ${vehicle.name}`);
+                    }
+                }
+                ;
+                console.log(`number of vehicles: ${vehicles.length}`);
+                break;
+            case 2:
+                for (let i = 0; i < vehicles.length; i++) {
+                    let vehicle = vehicles[i];
+                    console.log(vehicle);
+                }
+                break;
+            case 3:
+                for (let i = 0; i < vehicles.length; i++) {
+                    let vehicle = eVehicles[i];
+                    if (vehicle) {
+                        if (vehicle.battery >= 20 && vehicle.isRented === false) {
+                            console.log(`${vehicle.type} - ${vehicle.name} - ${vehicle.battery}`);
+                        }
+                    }
+                }
+                break;
+            case 4:
+                for (let i = 0; i < vehicles.length; i++) {
+                    let vehicle = eVehicles[i];
+                    if (vehicle) {
+                        if (vehicle.battery != 100 && vehicle.isRented === false) {
+                            console.log(`charged for ${100 - vehicle.battery} to 100 %:\n ${vehicle.type} - ${vehicle.name} - ${vehicle.battery}`);
+                        }
+                    }
+                }
+                break;
+            case 5:
+                break;
+        }
+    }
 }
+function logRentType(type, price) {
+    let isTypeNull = true;
+    // gucken, ob "a" oder "an" benutzt werden muss
+    if (type === "e-scooter" || "e-bike") {
+        console.log(`You want to rent an ${type}, is that right? That would be ${price} € per minute.`);
+    }
+    if (type === "bike") {
+        console.log(`You want to rent a ${type}, is that right? That would be ${price} € per minute.`);
+    }
+    while (isTypeNull) {
+        let yesOrNo = readlineSync.question("[y]/[n]\nchoice: ");
+        if (yesOrNo === null) {
+            console.log("Please type 'y' for yes or 'n' for no");
+            continue;
+        }
+        if (yesOrNo.toLowerCase() === "y") {
+            console.log("You voted yes!");
+            isTypeNull = false;
+        }
+        else if (yesOrNo.toLowerCase() === "n") {
+            console.log("You voted no!");
+            isTypeNull = false;
+        }
+    }
+}
+function logReturnType(name, price) {
+    console.log(`You want to return your ${name}.`);
+    let isTypeNullD = true;
+    while (isTypeNullD) {
+        let duration = readlineSync.question(`How long did you rent your ${name}?\nduration in minutes: `);
+        if (duration === null || isNaN(parseInt(duration))) {
+            console.log("Please enter a whole number.");
+            continue;
+        }
+        const num = parseInt(duration);
+        console.log(`\nYou have successfully returned your ${name} and will be charged ${num * price} from your account.\nThank you for being with us!`);
+        isTypeNullD = false;
+    }
+}
+function rentVehicle(customer) {
+    let isTypeNullB = true;
+    while (isTypeNullB) {
+        let typeOfVehicle = readlineSync.question(`\nHi, ${customer}!\nWhat kind of vehicle do you want to rent today?\n[1] e-scooter\n[2] e-bike\n[3] bike\n`);
+        // Antwort parsen und auswerten
+        const num = parseInt(typeOfVehicle);
+        if (typeOfVehicle === null || isNaN(num) || num < 1 || num > 3) {
+            console.log("Please enter 1, 2, or 3.");
+            continue;
+        }
+        isTypeNullB = false;
+        switch (num) {
+            case 1:
+                logRentType("e-scooter", EScooter.pricePerMinute);
+                break;
+            case 2:
+                logRentType("e-bike", EBike.pricePerMinute);
+                break;
+            case 3:
+                logRentType("bike", Bike.pricePerMinute);
+                break;
+        }
+    }
+}
+function returnVehicle(customer) {
+    let isTypeNullC = true;
+    while (isTypeNullC) {
+        let typeOfVehicle = readlineSync.question(`\nHi, ${customer}!\nWhat kind of vehicle do you want to return today?\n[1] e-scooter\n[2] e-bike\n[3] bike\n`);
+        // Antwort parsen und auswerten
+        const num = parseInt(typeOfVehicle);
+        if (typeOfVehicle === null || isNaN(num) || num < 1 || num > 3) {
+            console.log("Please enter 1, 2, or 3.");
+            continue;
+        }
+        isTypeNullC = false;
+        switch (num) {
+            case 1:
+                logReturnType("e-scooter", EScooter.pricePerMinute);
+                break;
+            case 2:
+                logReturnType("e-bike", EBike.pricePerMinute);
+                break;
+            case 3:
+                logReturnType("bike", Bike.pricePerMinute);
+                break;
+        }
+    }
+}
+function startScreen(customer) {
+    let isTypeNullA = true;
+    while (isTypeNullA) {
+        let whatToDo = readlineSync.question(`Welcome, ${customer}!\nWhat do you want to do?\n[1] rent a vehicle\n[2] return a vehicle\n[3]go to admin page\n`);
+        const num = parseInt(whatToDo);
+        if (whatToDo === null || isNaN(num) || num < 1 || num > 3) {
+            console.log("Please enter 1, 2, or 3.");
+            continue;
+        }
+        isTypeNullA = false;
+        switch (num) {
+            case 1:
+                rentVehicle(customer);
+                break;
+            case 2:
+                returnVehicle(customer);
+                break;
+            case 3:
+                adminScreen(customer);
+                break;
+        }
+    }
+}
+// ++++ Ausgeführter Code beginnt hier ++++
 logo();
 const customer = readlineSync.question("Hi! What should I call you?\nname: ");
 console.log(`Okay, thank you, ${customer}!`);
-let isTypeNull = true;
-while (isTypeNull) {
-    let typeOfVehicle = readlineSync.question("\nWhat kind of vehicle do you want to rent today?\n[1] e-scooter\n[2] e-bike\n[3] bike\n");
-    // Antwort parsen und auswerten
-    if (typeOfVehicle === null || isNaN(parseInt(typeOfVehicle))) {
-        continue;
-    }
-    isTypeNull = false;
-    const num = parseInt(typeOfVehicle);
-    try {
-        switch (num) {
-            case 1:
-                logType("e-scooter");
-                break;
-            case 2:
-                logType("e-bike");
-                break;
-            case 3:
-                logType("bike");
-                break;
-            default:
-                console.log("Please enter 1, 2, or 3");
-        }
-    }
-    catch (error) {
-        console.log(error);
-    }
-}
+startScreen(customer);
 //# sourceMappingURL=test.js.map
